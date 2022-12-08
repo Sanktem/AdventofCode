069c2ac43bd0f760f17b270ec8b85559d849edce
Dale Scott <55069993+Sanktem@users.noreply.github.com>
2022-12-07 18:20:52 -0600

    modified:   Day7/day7.py 	deleted:    Day7/output.txt

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,27 +1,52 @@
 #Advent Of Code Day 7
 
-from array import *
-import os
+from collections import defaultdict
+from math import inf
+from os import path, sep
 
 def PartOne():
-    tab = '    '
-    with open ('data.txt', 'r') as data, open ('output.txt', 'a') as output:
-        line = data.readlines()
-        i = 0
-        for command in line:
-            cmd = command.split(' ')
-            if (cmd[0] == '$') and (cmd[1] == 'cd'):
-                 print(y)
-
-
-
+    p1 = 0
+    cwd = ''
+    dir_sizes = defaultdict(int)
+    with open('./data.txt', 'r') as f:
+        for line in f:
+            cmd = line.strip().split(' ')
+            if cmd[0] == '$' and cmd[1] == 'cd':
+                cwd = path.normpath(path.join(cwd, cmd[2]))
             
+            if cmd[0].isnumeric():
+                dirs = cwd.split(sep)
+                for i in range(len(dirs)):
+                    dir_path = path.normpath(path.join(*dirs[: i + 1]))
+                    dir_sizes[dir_path] += int(cmd[0])
+    
+    sizes = dir_sizes.values()
 
+    p1 = sum((v for v in sizes if v <= 1e5))
 
-def test(): 
-    test_list = [[[1,2,3],[1,2,'this one'],[1,2,3]], [1,2,3]]
-    print(test_list[0][1][2])
+    return p1
 
-os.remove('output.txt')
-PartOne()
-#test()
+def PartTwo():
+    p2 = 0
+    cwd = ''
+    dir_sizes = defaultdict(int)
+    with open('./data.txt', 'r') as f:
+        for line in f:
+            cmd = line.strip().split(' ')
+            if cmd[0] == '$' and cmd[1] == 'cd':
+                cwd = path.normpath(path.join(cwd, cmd[2]))
+            
+            if cmd[0].isnumeric():
+                dirs = cwd.split(sep)
+                for i in range(len(dirs)):
+                    dir_path = path.normpath(path.join(*dirs[: i + 1]))
+                    dir_sizes[dir_path] += int(cmd[0])
+    
+    avail_space = 7e7 - dir_sizes["."]
+    sizes = dir_sizes.values()
+    p2 = min((v for v in sizes if v + avail_space >= 3e7))
+    
+    return p2
+
+print('Part one is: ', PartOne())
+print('Part two is: ', PartTwo())
