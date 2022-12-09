e760c7f769645d438602d6c153ace726dd8fcac5
Dale Scott <55069993+Sanktem@users.noreply.github.com>
2022-12-08 22:52:03 -0600

    part two compleate

--- Day8/day8.py
+++ Day8/day8.py
@@ -3,43 +3,50 @@
 from array import *
 
 def PartOne():
-    with open ('./data.txt', 'r') as f:
-        line = f.read().splitlines()
-        grid = []
-        score = 0
-        for row_one in line:
-            tree = row_one.split()
-            grid.append([*map (int, list(tree[0]))])
-        
-        for i in range(len(grid[0])):
-            for j in range(len(grid)):
-                tree = grid[i][j]
-                if all(x < tree for x in grid[i][0:j]) or \
-                    all(x < tree for x in grid[i][j+1:]) or \
-                    all(x < tree for x in grid[j][0:i]) or \
-                    all(x < tree for x in grid[j][i+1:]):
-                    score += 1
+    data = open('data.txt').readlines()
+
+    grid = [[int(x) for x in row.strip()] for row in data]
+    grid2 = list(zip(*grid))
     
-    return score - 20
+    score = 0
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            tree = grid[i][j]
+            if all(x < tree for x in grid[i][0:j]) or \
+                all(x < tree for x in grid[i][j+1:]) or \
+                all(x < tree for x in grid2[j][0:i]) or \
+                all(x < tree for x in grid2[j][i+1:]):
+                score += 1
+    
+    return score
+
+def view_length(tree, view):
+    view_length = 0
+    for v in view:
+        view_length += 1
+        if v >= tree:
+            break
+    return view_length
 
 def PartTwo():
-    with open ('./data.txt', 'r') as f:
-        line = f.read().splitlines()
-        grid = []
-        score = 0
-        for row_one in line:
-            tree = row_one.split()
-            grid.append([*map (int, list(tree[0]))])
-        
-        for i in range(len(grid[0])):
-            for j in range(len(grid)):
-                tree = grid[i][j]
-                if all(x < tree for x in grid[i][0:j]) or \
-                    all(x < tree for x in grid[i][j+1:]) or \
-                    all(x < tree for x in grid[j][0:i]) or \
-                    all(x < tree for x in grid[j][i+1:]):
-                    score += 1
+    data = open('data.txt').readlines()
+
+    grid = [[int(x) for x in row.strip()] for row in data]
+    grid2 = list(zip(*grid))
     
-    return score - 20
+    s = 0
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            tree = grid[i][j]
+            s1 = view_length(tree, grid[i][0:j][::-1])
+            s2 = view_length(tree, grid[i][j+1:])
+            s3 = view_length(tree, grid2[j][0:i][::-1])
+            s4 = view_length(tree, grid2[j][i+1:])
+            score = s1 * s2 * s3 * s4
+            if score > s:
+                 s = score
+    
+    return s
 
 print('Part One is: ', PartOne())
+print('Part two is: ', PartTwo())
