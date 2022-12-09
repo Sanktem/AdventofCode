5b1142720b9efdfbe193839c4e7b50dc4bbac552
Dale Scott <55069993+Sanktem@users.noreply.github.com>
2022-12-08 22:44:55 -0600

    Part one compleated need to still work on part 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -2,12 +2,44 @@
 
 from array import *
 
-with open ('./data.txt', 'r') as f:
-    line = f.read().splitlines()
-    grid = []
-    for row_one in line:
-        tree = row.split()
-        grid.append(list(tree[0]))
+def PartOne():
+    with open ('./data.txt', 'r') as f:
+        line = f.read().splitlines()
+        grid = []
+        score = 0
+        for row_one in line:
+            tree = row_one.split()
+            grid.append([*map (int, list(tree[0]))])
+        
+        for i in range(len(grid[0])):
+            for j in range(len(grid)):
+                tree = grid[i][j]
+                if all(x < tree for x in grid[i][0:j]) or \
+                    all(x < tree for x in grid[i][j+1:]) or \
+                    all(x < tree for x in grid[j][0:i]) or \
+                    all(x < tree for x in grid[j][i+1:]):
+                    score += 1
+    
+    return score - 20
 
+def PartTwo():
+    with open ('./data.txt', 'r') as f:
+        line = f.read().splitlines()
+        grid = []
+        score = 0
+        for row_one in line:
+            tree = row_one.split()
+            grid.append([*map (int, list(tree[0]))])
+        
+        for i in range(len(grid[0])):
+            for j in range(len(grid)):
+                tree = grid[i][j]
+                if all(x < tree for x in grid[i][0:j]) or \
+                    all(x < tree for x in grid[i][j+1:]) or \
+                    all(x < tree for x in grid[j][0:i]) or \
+                    all(x < tree for x in grid[j][i+1:]):
+                    score += 1
+    
+    return score - 20
 
-print(len(grid[0]))
+print('Part One is: ', PartOne())
